a335acf9004923b2db8f2e90f6ab1cb5d524b573
JenChieh <jcs090218@gmail.com>
2024-01-01 01:44:06 -0800

    chore: Update copyright year

--- codemetrics-rules.el
+++ codemetrics-rules.el
@@ -1,6 +1,6 @@
 ;;; codemetrics-rules.el --- Define scoring rules  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; This file is not part of GNU Emacs.
 
--- codemetrics.el
+++ codemetrics.el
@@ -1,6 +1,6 @@
 ;;; codemetrics.el --- Plugin shows complexity information  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2023  Shen, Jen-Chieh
+;; Copyright (C) 2023-2024  Shen, Jen-Chieh
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
