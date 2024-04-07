440017c4c97e90fcb5727f0f99d517cb3c4495f3
Marie Katrine Ekeberg <mke@themkat.net>
2024-04-07 18:12:57 +0200

    Simple test case for C language

---
+++ test/c-test.el
@@ -0,0 +1,37 @@
+;;; c-test.el --- C language tests for codemetrics.el  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 Marie Katrine Ekeberg
+
+;; Author: Marie Katrine Ekeberg <mke@themkat.net>
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+
+;;; Code:
+(require 'codemetrics)
+
+(codemetrics-test c-simple
+  "test/c/Simple.c"
+  c-mode
+  '(4
+    (preproc_ifdef . 1)
+    (function_definition . 0)
+    (for_statement . 1)
+    (while_statement . 1)
+    (if_statement . 1)
+    (call_expression . 0)))
+
+;;; c-test.el ends here
